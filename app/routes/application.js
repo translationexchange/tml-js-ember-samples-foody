88cf00889ff3dae395d9097b8b84cfec56c1f9eb
Ian McDaniel <ianmcdainel@gmail.com>
2017-08-10 14:22:58 -0700

    added initalize function for new tml service

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  beforeModel(){
+    return this.get('tml').initialize();
+  },
+
+
   model(){
     return Ember.$.getJSON('/api/data.json');
   }
